b85444f65a0b30c14b742768eab5f18d88abcd6d
Aleksey Semenov <7.on.off@bk.ru>
2020-10-21 16:05:42 +0300

    fix: test commit

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,6 @@ import AppController from './AppController';
 
 /* eslint no-unused-vars: 0 */
 
-const container = document.querySelector('.wrapper-page.container');
+const container = document.querySelector(' .wrapper-page.container');
 const controller = new AppController(container);
 controller.init();
